502a0b6dc6a75dfb6386f2a1f961484089c6a897
ancourt <ancourt@z3r11p5.42lyon.fr>
2026-03-17 14:58:03 +0100

    update

--- Module_01/ex3/ft_plant_factory.py
+++ Module_01/ex3/ft_plant_factory.py
@@ -7,3 +7,10 @@ class Plant:
         self.starting_age = starting_age
         Plant.Garden.append(self)
 
+if __name__ == "__main__":
+    while True:
+        name = input("\nWrite:\n'plant name' to add a new plant")
+        starting_height = float(input("Enter height (cm): "))
+        starting_age = int(input("Enter age (days): "))
+        new_plant = Plant(name, height, age_plant)
+
