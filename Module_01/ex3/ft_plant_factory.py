8b33b780841ab1e48b143c9aca48006f115dffe9
ancourt <ancourt@z4r9p1.42lyon.fr>
2026-03-26 13:14:23 +0100

    update

--- Module_01/ex3/ft_plant_factory.py
+++ Module_01/ex3/ft_plant_factory.py
@@ -1,20 +1,51 @@
+def uppercase(seed_type: str) -> str:
+    table = {
+        'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D', 'e': 'E',
+        'f': 'F', 'g': 'G', 'h': 'H', 'i': 'I', 'j': 'J',
+        'k': 'K', 'l': 'L', 'm': 'M', 'n': 'N', 'o': 'O',
+        'p': 'P', 'q': 'Q', 'r': 'R', 's': 'S', 't': 'T',
+        'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X', 'y': 'Y',
+        'z': 'Z'
+    }
+    if seed_type[0] in table:
+        first = table[seed_type[0]]
+    else:
+        first = seed_type[0]
+    return first + seed_type[1:]
+
 class Plant:
     Garden: list["Plant"] = []
 
-    def __init__(self, name: str, starting_height: float, starting_age: int):
+    def __init__(self, name: str, height: float, age_plant: int):
         self.name = name
-        self.starting_height = starting_height
-        self.starting_age = starting_age
+        self.height = height
+        self.age_plant = age_plant
         Plant.Garden.append(self)
 
-if __name__ == "__main__":
-    while True:
-        name = input("\nWrite:\n'plant name' to add a new plant or 'stop' to quit")
-        if name == "stop": 
-            break;
-        else:
-            starting_height = float(input("Enter height (cm): "))
-            starting_age = int(input("Enter age (days): "))
-            new_plant = Plant(name, starting_height, starting_age)
-            
+    def __str__(self):
+        return f"{uppercase(self.name)}: {self.height}cm, {self.age_plant} days old"
 
+    def get_info(self):
+        print(self)
+
+def plant_factory(plants_data: list):
+    plant_list = []
+    for plant in plants_data:
+        for name, height, age in plant:
+            plant_list.append(Plant(name, height, age))
+
+if __name__ == "__main__":
+    plants_data = [
+        ("rose", 12.0, 0),
+        ("tomatoe", 14.0, 3),
+        ("fern", 4.2, 2),
+        ("poppy", 6.0, 1),
+        ("carrot", 11.0, 3),
+        ("clover", 8.2, 2),
+    ]
+
+    plant_factory(plants_data)
+
+    print("=== Plant factory output ===")
+    for plant in Plant.Garden: 
+        plant.get_info()
