7f51048380fb32f84ea589e4982ca951d9ecd35d
anaiscourt2-source <anaiscourt2@gmail.com>
2026-04-04 21:19:35 +0200

    update on 2 ex

--- Module_01/ex0/ft_garden_intro.py
+++ Module_01/ex0/ft_garden_intro.py
@@ -1,13 +1,12 @@
 def ft_garden_intro() -> None:
-	print("=== Welcome to My Garden ===")
-	name = "Poppy"
-	height = 15
-	age = 5
-	print("Plant:", name)
-	print("Height:", height)
-	print("Age:", age)
-	print("\n")
-	print("=== End of Program ===")
+    print("=== Welcome to My Garden ===")
+    name: str = "Poppy"
+    height: int = 15
+    age: int = 5
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days old")
+    print("\n=== End of Program ===")
 
 if __name__ == "__main__":
 	ft_garden_intro()
