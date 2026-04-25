5ae256efecff94b487c5eef87c7eff537d5eb995
ancourt <ancourt@z4r10p5.42lyon.fr>
2026-04-25 17:20:51 +0200

    ex0

--- Module_05/ex0/ft_data_processor.py
+++ Module_05/ex0/ft_data_processor.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+class DataProcessor(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def validate(self, data):
+        isinstance(data, Any)
+
+    @abstractmethod
+    def ingest(self, data):
+        pass
+
+class NumericProcessor(DataProcessor):
+    def __init__(self):
+        super().__init__()
+
+    def validate(self, data):
+        return isinstance(data, (int, float))
+
+    def ingest(self, data):
+        if self.validate(data):
+            return data
+        else:
+            raise ValueError("Invalid data type")
+
+class TextProcessor(DataProcessor):
+    def __init__(self):
+        super().__init__()
+
+    def validate(self, data):
+        return isinstance(data, str)
+
+    def ingest(self, data):
+        if self.validate(data):
+            return data
+        else:
+            raise ValueError("Invalid data type")
+
+class NumericProcessor(LogProcessor):
